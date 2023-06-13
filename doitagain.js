e15718ddf237b2114edf7276699eebc763e324b4
Yaslin Carmona <yaslincarmona@Yaslins-MacBook-Air.local>
2023-06-13 15:59:33 -0400

    loops

---
+++ doitagain.js
@@ -0,0 +1,22 @@
+function myArray () {
+    const numChild = [1,2,3,4,5,6,7,8];
+
+    for (let i = 0; i < numChild.length; i++) {
+        const result = numChild[i]
+        console.log (result);
+    }
+}
+
+myArray()
+
+
+function arrayCounter (array) {
+    for(let i = 0; i < array.length; i++) {
+        const result = [i];
+        console.log[result];
+    }
+}
+
+const myShirt = ["cotton","silk"];
+const myLorezo = ["schalltt", "drift"]
+arrayCounter(myLorezo);
---
+++ index.js
@@ -0,0 +1,34 @@
+//function firstLoop(total) {
+////const total = 20 Remove this to make it scalable and then put the number in the () at the bottom. 
+//
+//for (let i = 0; i < total; i++) {
+//    console.log(i)
+//}
+//}
+//firstLoop(2000)
+//
+//function fingerLoop() {
+//const totalFinger = 10;
+//
+//for (let i = 0; i < totalFinger; i ++) {
+//    let sentence = "I have " + (i+1) + " fingers";
+//    console.log(sentence);
+//}
+//}
+
+//^This is how you will start at 1 instead of 0.
+
+//function arrayLoop(array) {
+//    const prime = [2,3,5,7,11,13,17,19,23,29,31,31,37,41,43,47]
+//    for (let i = 47; i < prime.length; i--) {
+//        const result = prime[i];
+//        console.log(result);
+//}
+//}
+//arrayLoop();
+//
+//
+//const primeArray = [2,3,5,7,11,13,17,19,23,29,31,31,37,41,43,47]
+//arrayLoop(primeArray);
+//
+//
